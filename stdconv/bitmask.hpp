3108925ee979a7d4ebeaf383070fd3d94f99cdad
Lukasz Michalski <lm@zork.pl>
2019-09-14 20:54:50 +0200

    added single register bitmask converter

---
+++ stdconv/bitmask.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cmath>
+#include "libmodmqttconv/converter.hpp"
+
+class BitmaskConverter : public IStateConverter {
+    public:
+        virtual MqttValue toMqtt(const ModbusRegisters& data) const {
+            int val = data.getValue(0) & mask;
+            return MqttValue::fromInt(val);
+        }
+
+        virtual void setArgs(const std::vector<std::string>& args) {
+            mask = getHex16Arg(0, args);
+        }
+
+        virtual ~BitmaskConverter() {}
+    private:
+        u_int16_t mask = 0xffff;
+};
