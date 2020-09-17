0ba09e52a213c1c8a1c6b6eb6856d2597fcd82ce
Lukasz Michalski <lm@zork.pl>
2020-09-17 21:48:40 +0200

    exprtk support - before IStateConverter.toMqtt const removal

--- exprconv/plugin.cpp
+++ exprconv/plugin.cpp
@@ -1,9 +1,10 @@
-
-#include <exprtk.hpp>
 #include "plugin.hpp"
-
+#include "expr.hpp"
 
 IStateConverter*
 StdConvPlugin::getStateConverter(const std::string& name) {
+    if(name == "expr") {
+        return new ExprtkConverter();
+    }
     return nullptr;
 }
