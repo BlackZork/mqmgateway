b5f17ac03efc038c32716887c5567c462b856e84
Lukasz Michalski <lm@zork.pl>
2022-12-17 23:27:40 +0100

    Added test for exprtk scalar value conversion - example of converting uint16_t to int16_t

---
+++ unittests/mqtt_unnamed_scalar_expr_tests.cpp
@@ -0,0 +1,39 @@
+#include "catch2/catch.hpp"
+#include "mockedserver.hpp"
+#include "defaults.hpp"
+
+static const std::string config = R"(
+modmqttd:
+  loglevel: 5
+  converter_search_path:
+    - build/exprconv
+  converter_plugins:
+    - exprconv.so
+modbus:
+  networks:
+    - name: tcptest
+      address: localhost
+      port: 501
+mqtt:
+  client_id: mqtt_test
+  refresh: 1s
+  broker:
+    host: localhost
+  objects:
+    - topic: test_sensor
+      state:
+        register: tcptest.1.2
+        register_type: input
+        converter: expr.evaluate("R0 - 65536")
+)";
+
+TEST_CASE ("Expression on unnamed scalar should be evaluated") {
+    MockedModMqttServerThread server(config);
+    int16_t negative = -20;
+    server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::INPUT, negative);
+    server.start();
+    // default mocked modbus read time is 5ms per register
+    server.waitForPublish("test_sensor/state", REGDEBUG_MSEC);
+    REQUIRE(server.mqttValue("test_sensor/state") == "-20");
+    server.stop();
+}
