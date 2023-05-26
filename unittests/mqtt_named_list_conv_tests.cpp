6043699e6153926292e1816f082051e9ef1942e5
Lukasz Michalski <lm@zork.pl>
2023-05-26 14:14:15 +0200

    tests for state list declarations as register and count

--- unittests/mqtt_named_list_conv_tests.cpp
+++ unittests/mqtt_named_list_conv_tests.cpp
@@ -3,7 +3,7 @@
 #include "jsonutils.hpp"
 #include "defaults.hpp"
 
-static const std::string config = R"(
+static const std::string config1 = R"(
 modmqttd:
   converter_search_path:
     - build/stdconv
@@ -32,7 +32,7 @@ mqtt:
 )";
 
 TEST_CASE ("Named state list should output converted value") {
-    MockedModMqttServerThread server(config);
+    MockedModMqttServerThread server(config1);
     server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::INPUT, 2);
     server.setModbusRegisterValue("tcptest", 1, 3, modmqttd::RegisterType::INPUT, 1);
     server.start();
@@ -46,5 +46,50 @@ TEST_CASE ("Named state list should output converted value") {
     //2^17 + 1
     REQUIRE_JSON(server.mqttValue("test_state/state"), "{\"some_name\": 131073}");
     REQUIRE(server.mModbusFactory->getMockedModbusContext("tcptest").getReadCount(1) == 2);
+    server.stop();
+}
+
+
+static const std::string config2 = R"(
+modmqttd:
+  converter_search_path:
+    - build/stdconv
+  converter_plugins:
+    - stdconv.so
+modbus:
+  networks:
+    - name: tcptest
+      address: localhost
+      port: 501
+mqtt:
+  client_id: mqtt_test
+  refresh: 1s
+  broker:
+    host: localhost
+  objects:
+    - topic: test_state
+      state:
+        name: some_name
+        converter: std.int32()
+          register: tcptest.1.2
+          register_type: input
+          count: 2
+)";
+
+TEST_CASE ("Named state list should output converted value with single modbus read call") {
+    MockedModMqttServerThread server(config1);
+    server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::INPUT, 2);
+    server.setModbusRegisterValue("tcptest", 1, 3, modmqttd::RegisterType::INPUT, 1);
+    server.start();
+
+    //to make sure that all registers have initial value
+    server.waitForPublish("test_state/availability");
+    REQUIRE(server.mqttValue("test_state/availability") == "1");
+
+    server.waitForPublish("test_state/state");
+
+    //2^17 + 1
+    REQUIRE_JSON(server.mqttValue("test_state/state"), "{\"some_name\": 131073}");
+    REQUIRE(server.mModbusFactory->getMockedModbusContext("tcptest").getReadCount(1) == 1);
     server.stop();
 }
