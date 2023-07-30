5343d4c1a7a64fd2a0664ee1cdef895d4b138532
Lukasz Michalski <lm@zork.pl>
2023-07-30 09:55:42 +0200

    fix for uint32 publish, closes #25

--- unittests/mqtt_unnamed_scalar_conv_tests.cpp
+++ unittests/mqtt_unnamed_scalar_conv_tests.cpp
@@ -2,9 +2,11 @@
 #include "mockedserver.hpp"
 #include "defaults.hpp"
 
+
+TEST_CASE ("Unnamed scalar should output converted value") {
+
 static const std::string config = R"(
 modmqttd:
-  loglevel: 5
   converter_search_path:
     - build/stdconv
   converter_plugins:
@@ -27,12 +29,51 @@ mqtt:
         converter: std.divide(1000,3)
 )";
 
-TEST_CASE ("Unnamed scalar should output converted value") {
+
     MockedModMqttServerThread server(config);
     server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::INPUT, 32456);
     server.start();
     // default mocked modbus read time is 5ms per register
     server.waitForPublish("test_sensor/state");
     REQUIRE(server.mqttValue("test_sensor/state") == "32.456");
+    server.stop();
+}
+
+
+TEST_CASE ("Uint32 value should be published") {
+
+static const std::string config = R"(
+modmqttd:
+  converter_search_path:
+    - build/stdconv
+  converter_plugins:
+    - stdconv.so
+modbus:
+  networks:
+    - name: tcptest
+      address: localhost
+      port: 501
+mqtt:
+  client_id: mqtt_test
+  refresh: 1s
+  broker:
+    host: localhost
+  objects:
+    - topic: test_sensor
+      state:
+        register: tcptest.1.2
+        count: 2
+        register_type: input
+        converter: std.uint32()
+)";
+
+
+    MockedModMqttServerThread server(config);
+    server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::INPUT, 0x9234);
+    server.setModbusRegisterValue("tcptest", 1, 3, modmqttd::RegisterType::INPUT, 0xABCD);
+    server.start();
+    // default mocked modbus read time is 5ms per register
+    server.waitForPublish("test_sensor/state");
+    REQUIRE(server.mqttValue("test_sensor/state") == "2452925389");
     server.stop();
 }
