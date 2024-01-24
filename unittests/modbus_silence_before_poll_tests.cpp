eb3b4da2bd61e344ad2fbeb35852970eb505dad8
Lukasz Michalski <lm@zork.pl>
2024-01-24 23:24:17 +0100

    test for ignoring silence period

--- unittests/modbus_silence_before_poll_tests.cpp
+++ unittests/modbus_silence_before_poll_tests.cpp
@@ -45,5 +45,26 @@ mqtt:
 
         server.stop();
     }
+
+    SECTION("should be ignored when next poll is later") {
+        config.mYAML["mqtt"]["objects"][0]["state"]["refresh"] = "25ms";
+        MockedModMqttServerThread server(config.toString());
+        server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 1);
+        server.start();
+        server.waitForPublish("test_sensor/state");
+        REQUIRE(server.mqttValue("test_sensor/state") == "1");
+        std::chrono::time_point<std::chrono::steady_clock> first_poll_ts = server.getLastPollTime();
+
+        // we should poll in 25ms, ignoring 15ms silence period
+        server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 2);
+        server.waitForPublish("test_sensor/state");
+        auto ptime = server.getLastPollTime() - first_poll_ts;
+        // add 5ms for poll time, 5ms for code execution
+        REQUIRE(ptime < std::chrono::milliseconds(35));
+        REQUIRE(server.mqttValue("test_sensor/state") == "2");
+
+        server.stop();
+    }
+
 }
 
