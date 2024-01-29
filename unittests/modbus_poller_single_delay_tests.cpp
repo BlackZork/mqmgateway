bcdc1ba4356f319483e8c2f03921bf050bea88dc
Lukasz Michalski <lm@zork.pl>
2024-01-29 23:02:49 +0100

    tests for whole ModbusThread poll logic when delay_before_first_poll is set

--- unittests/modbus_poller_single_delay_tests.cpp
+++ unittests/modbus_poller_single_delay_tests.cpp
@@ -77,4 +77,42 @@ TEST_CASE("ModbusPoller for single delay config") {
         REQUIRE(poller.allDone());
     }
 
+    SECTION("should not delay next poll if slave was the same") {
+        poller.setSlaveDelay(1, std::chrono::milliseconds(15));
+        poller.setSlaveDelay(2, std::chrono::milliseconds(20));
+
+        //initial poll selects reg2 due to longer delay needed
+        poller.setupInitialPoll(registers);
+        waitTime = poller.pollNext();
+        REQUIRE(modbus_factory.getLastReadRegisterAddress() == std::tuple(2,20));
+        REQUIRE(waitTime == std::chrono::milliseconds::zero());
+        REQUIRE(!poller.allDone());
+
+        waitTime = poller.pollNext();
+        REQUIRE(modbus_factory.getLastReadRegisterAddress() == std::tuple(1,1));
+        REQUIRE(poller.allDone());
+
+        //make silence that can acomodate both delays
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
+
+        poller.setPollList(registers);
+        waitTime = poller.pollNext();
+        REQUIRE(waitTime == std::chrono::milliseconds::zero());
+        //we choose to poll slave 1 because it was polled last
+        REQUIRE(modbus_factory.getLastReadRegisterAddress() == std::tuple(1,1));
+        REQUIRE(!poller.allDone());
+
+        //reg2 needs silence due to slave change
+        waitTime = poller.pollNext();
+        REQUIRE(modbus_factory.getLastReadRegisterAddress() == std::tuple(2,20));
+        REQUIRE(waitTime > std::chrono::milliseconds(10));
+        REQUIRE(!poller.allDone());
+
+        //make silence that can acomodate reg2
+        std::this_thread::sleep_for(std::chrono::milliseconds(25));
+
+        waitTime = poller.pollNext();
+        REQUIRE(modbus_factory.getLastReadRegisterAddress() == std::tuple(2,20));
+        REQUIRE(poller.allDone());
+    }
 }
