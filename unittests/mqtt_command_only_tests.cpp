86aff906f9b76ba29c15a8ff85aab521f710335b
Lukasz Michalski <lm@zork.pl>
2024-05-05 18:23:14 +0200

    added test for delay_before_first_command on network level

--- unittests/mqtt_command_only_tests.cpp
+++ unittests/mqtt_command_only_tests.cpp
@@ -55,12 +55,15 @@ TEST_CASE ("Write should respect slave delay_before_command") {
 }
 
 
+TEST_CASE ("When network delay") {
+
+
 static const std::string global_delay_config = R"(
 modmqttd:
 modbus:
-    delay_before_command: 500ms
     networks:
     - name: tcptest
+      delay_before_command: 500ms
       address: localhost
       port: 501
 mqtt:
@@ -75,7 +78,27 @@ mqtt:
           register_type: holding
 )";
 
-TEST_CASE ("Write should respect global delay_before_command") {
+SECTION("is set to delay_before_command write should wait") {
+    MockedModMqttServerThread server(global_delay_config);
+    server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 0);
+    server.start();
+
+    server.waitForSubscription("test_switch/set");
+
+    auto now = std::chrono::steady_clock::now();
+    server.publish("test_switch/set", "7");
+    server.publish("test_switch/set", "8");
+    server.waitForModbusValue("tcptest",1,2, modmqttd::RegisterType::HOLDING, 0x8, std::chrono::milliseconds(700));
+    auto after = std::chrono::steady_clock::now();
+
+    REQUIRE(after - now >= std::chrono::milliseconds(500));
+
+    server.stop();
+}
+
+SECTION("is set to delay_before_first_command write should wait") {
+    std::string config = std::regex_replace(global_delay_config, std::regex("delay_before_command"), "delay_before_first_command");
+
     MockedModMqttServerThread server(config);
     server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 0);
     server.start();
@@ -87,8 +110,11 @@ TEST_CASE ("Write should respect global delay_before_command") {
     server.publish("test_switch/set", "8");
     server.waitForModbusValue("tcptest",1,2, modmqttd::RegisterType::HOLDING, 0x8, std::chrono::milliseconds(700));
     auto after = std::chrono::steady_clock::now();
-    //global is ignored
+
     REQUIRE(after - now >= std::chrono::milliseconds(500));
 
     server.stop();
+}
+
+
 }
