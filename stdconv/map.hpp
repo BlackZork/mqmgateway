2dc9e27a460a697515f41981e840d168a84597d6
Lukasz Michalski <lm@zork.pl>
2024-07-15 17:38:15 +0200

    Protect unsave MapConverter::Mapping from outside use

--- stdconv/map.hpp
+++ stdconv/map.hpp
@@ -6,12 +6,55 @@
 #include "libmodmqttconv/converter.hpp"
 
 class MapConverter : public DataConverter {
+    friend class MapParser;
     public:
+        virtual MqttValue toMqtt(const ModbusRegisters& data) const {
+            // TODO should be detected at config parse time, not runtime
+            // we need DataConverter::isSingleRegisterOnly() for this.
+            if (data.getCount() != 1)
+                throw ConvException("Cannot map multiple registers");
+
+            std::vector<Mapping>::const_iterator it = mMappings.findRegValue(data.getValue(0));
+            if (it == mMappings.end()) {
+                return MqttValue::fromInt(data.getValue(0));
+            }
+
+            if (it->isInt())
+                return MqttValue::fromInt(it->getIntMqttValue());
+            else
+                return MqttValue::fromString(it->getStrMqttValue());
+        }
+
+        virtual ModbusRegisters toModbus(const MqttValue& value, int registerCount) const {
+            if (registerCount != 1)
+                throw ConvException("Cannot map multiple registers");
+
+            std::vector<Mapping>::const_iterator it = mMappings.findVariant(value);
+            if (it == mMappings.end())
+                return Int16Converter().toModbus(value, registerCount);
+
+            return it->mRegisterValue;
+        }
+
+        virtual void setArgs(const std::vector<std::string>& args) {
+            if (args.size() != 1)
+                throw ConvException("This converter accepts a single argument only");
+
+            parseMap(args[0]);
+        }
+
+        virtual ~MapConverter() {
+        }
+    private:
         typedef union {
             char* mStr;
             int mInt;
         } MqttVariant;
-
+        /**
+         * Do not use outside Map
+         * destructor does not free memory allocated
+         * for string variant
+         */
         class Mapping {
             public:
                 Mapping(uint16_t registerValue, int mqttIntValue)
@@ -77,44 +120,6 @@ class MapConverter : public DataConverter {
                 }
         };
 
-        virtual MqttValue toMqtt(const ModbusRegisters& data) const {
-            // TODO should be detected at config parse time, not runtime
-            // we need DataConverter::isSingleRegisterOnly() for this.
-            if (data.getCount() != 1)
-                throw ConvException("Cannot map multiple registers");
-
-            std::vector<Mapping>::const_iterator it = mMappings.findRegValue(data.getValue(0));
-            if (it == mMappings.end()) {
-                return MqttValue::fromInt(data.getValue(0));
-            }
-
-            if (it->isInt())
-                return MqttValue::fromInt(it->getIntMqttValue());
-            else
-                return MqttValue::fromString(it->getStrMqttValue());
-        }
-
-        virtual ModbusRegisters toModbus(const MqttValue& value, int registerCount) const {
-            if (registerCount != 1)
-                throw ConvException("Cannot map multiple registers");
-
-            std::vector<Mapping>::const_iterator it = mMappings.findVariant(value);
-            if (it == mMappings.end())
-                return Int16Converter().toModbus(value, registerCount);
-
-            return it->mRegisterValue;
-        }
-
-        virtual void setArgs(const std::vector<std::string>& args) {
-            if (args.size() != 1)
-                throw ConvException("This converter accepts a single argument only");
-
-            parseMap(args[0]);
-        }
-
-        virtual ~MapConverter() {
-        }
-    private:
         // register value to mqtt value
         Map mMappings;
         void parseMap(const std::string& pStrMap);
