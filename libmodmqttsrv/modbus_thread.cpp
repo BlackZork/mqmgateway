5c56e77bfe02bf1af80ce3b9ff85072498f6e3cd
Lukasz Michalski <lm@zork.pl>
2024-08-23 23:04:26 +0200

    log queue mode for poll group set for modbus thread

--- libmodmqttsrv/modbus_thread.cpp
+++ libmodmqttsrv/modbus_thread.cpp
@@ -85,6 +85,7 @@ ModbusThread::setPollSpecification(const MsgRegisterPollSpecification& spec) {
             << ", register " << (*it)->mRegister << ":" << (*it)->mRegisterType
             << ", count=" << (*it)->getCount()
             << ", poll every " << std::chrono::duration_cast<std::chrono::milliseconds>((*it)->mRefresh).count() << "ms"
+            << ", queue " << ((*it)->mPublishMode == PublishMode::ON_CHANGE ? "on change" : "always")
             << ", min f_delay " << std::chrono::duration_cast<std::chrono::milliseconds>((*it)->getDelayBeforeFirstCommand()).count() << "ms"
             << ", min delay " << std::chrono::duration_cast<std::chrono::milliseconds>((*it)->getDelayBeforeCommand()).count() << "ms";
         }
