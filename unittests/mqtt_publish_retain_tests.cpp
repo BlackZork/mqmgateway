fe74bef7f136dfaebe047e398c331c87cb8e7ac1
Lukasz Michalski <lm@zork.pl>
2024-12-27 10:19:07 +0100

    Test for availability change in retain=false mode

--- unittests/mqtt_publish_retain_tests.cpp
+++ unittests/mqtt_publish_retain_tests.cpp
@@ -34,35 +34,62 @@ mqtt:
         REQUIRE(dur <  20);
     }
 
-    SECTION("is set to true then initial poll do not trigger publish") {
+    SECTION("is set to true") {
         config.mYAML["mqtt"]["objects"][0]["retain"] = "true";
-        MockedModMqttServerThread server(config.toString());
-        server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 1);
-        server.start();
-        server.waitForInitialPoll("tcptest");
-        // wait some time for mqttclient thread to process initial poll messages
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
-        server.requirePublishCount("test_sensor/state", 0);
 
-        server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 2);
-        server.waitForPublish("test_sensor/state");
-        REQUIRE(server.mqttValue("test_sensor/state") == "2");
+        SECTION("then initial poll do not trigger publish") {
+            MockedModMqttServerThread server(config.toString());
+            server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 1);
+            server.start();
+            server.waitForInitialPoll("tcptest");
+            // wait some time for mqttclient thread to process initial poll messages
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            server.requirePublishCount("test_sensor/state", 0);
+
+            server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 2);
+            server.waitForPublish("test_sensor/state");
+            REQUIRE(server.mqttValue("test_sensor/state") == "2");
+        }
+
+        SECTION("and publish mode is every_poll then initial poll triggers publish") {
+            config.mYAML["mqtt"]["refresh"] = "10ms";
+            config.mYAML["mqtt"]["objects"][0]["publish_mode"] = "every_poll";
+            MockedModMqttServerThread server(config.toString());
+            server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 2);
+            server.start();
+            //5 ms initail poll read, 10ms wait, 5ms second read, 5ms test tolerance
+            std::this_thread::sleep_for(std::chrono::milliseconds(25));
+            server.stop();
+
+            int test_count = server.mMqtt->getPublishCount("test_sensor/state");
+            REQUIRE(test_count == 2);
+        }
+
+        SECTION("then availability change do not publish old value") {
+            MockedModMqttServerThread server(config.toString());
+            server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 1);
+            server.start();
+            server.waitForPublish("test_sensor/availability");
+            REQUIRE(server.mqttValue("test_sensor/availability") == "1");
+
+            // do not publish 1 -> 2 change because availability is 0
+            server.setModbusRegisterReadError("tcptest", 1, 2, modmqttd::RegisterType::HOLDING);
+            server.waitForPublish("test_sensor/availability", defaultWaitTime() * 4);
+            REQUIRE(server.mqttValue("test_sensor/availability") == "0");
+
+            server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 2);
+            server.clearModbusRegisterReadError("tcptest", 1, 2, modmqttd::RegisterType::HOLDING);
+            // make sure that there is at least one sucessfull
+            // poll that is not published
+            std::this_thread::sleep_for(std::chrono::milliseconds(35));
+
+            server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 3);
+
+            server.waitForPublish("test_sensor/state");
+            REQUIRE(server.mqttValue("test_sensor/state") == "3");
+            // 2 -> 3 change only
+            server.requirePublishCount("test_sensor/state", 1);
+        }
     }
-
-    SECTION("is set to true and publish mode is every_poll then initial poll triggers publish") {
-        config.mYAML["mqtt"]["refresh"] = "10ms";
-        config.mYAML["mqtt"]["objects"][0]["retain"] = "true";
-        config.mYAML["mqtt"]["objects"][0]["publish_mode"] = "every_poll";
-        MockedModMqttServerThread server(config.toString());
-        server.setModbusRegisterValue("tcptest", 1, 2, modmqttd::RegisterType::HOLDING, 2);
-        server.start();
-        //5 ms initail poll read, 10ms wait, 5ms second read, 5ms test tolerance
-        std::this_thread::sleep_for(std::chrono::milliseconds(25));
-        server.stop();
-
-        int test_count = server.mMqtt->getPublishCount("test_sensor/state");
-        REQUIRE(test_count == 2);
-    }
-
 }
 
