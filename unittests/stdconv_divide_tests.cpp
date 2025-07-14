68623286d3462ddc7a0841e603c23629905104ac
Lukasz Michalski <lm@zork.pl>
2025-07-14 22:52:53 +0200

    Added divide tests with swap_bytes flag

--- unittests/stdconv_divide_tests.cpp
+++ unittests/stdconv_divide_tests.cpp
@@ -4,6 +4,8 @@
 
 #include "libmodmqttconv/converterplugin.hpp"
 
+#include "testnumbers.hpp"
+
 TEST_CASE("A number value should be divided") {
     std::string stdconv_path = "../stdconv/stdconv.so";
 
@@ -39,6 +41,18 @@ TEST_CASE("A number value should be divided") {
         REQUIRE(output.values() == std::vector<uint16_t>({0x2, 0x1}));
     }
 
+    SECTION("when one int32 byte swapped value is converted to two modbus registers, low byte first") {
+        MqttValue input(TestNumbers::Int::ABCD_as_int32);
+
+        args.push_back("0");
+        args.push_back("low_first");
+        args.push_back("swap_bytes");
+        conv->setArgs(args);
+
+        ModbusRegisters output = conv->toModbus(input, 2);
+
+        REQUIRE(output.values() == std::vector<uint16_t>({0xea61, 0xd9d0}));
+    }
 
     SECTION("when two modbus registers are converted into a string, high byte first") {
         ModbusRegisters input = ModbusRegisters(std::vector<uint16_t>{0x01, 0x2});
@@ -60,6 +74,20 @@ TEST_CASE("A number value should be divided") {
 
         REQUIRE(output.getString() == "32769");
     }
+
+
+    SECTION("when two modbus byte swapped registers are converted into a string, low byte first") {
+        ModbusRegisters input = ModbusRegisters(std::vector<uint16_t>{0x0201, 0x0804});
+
+        args.push_back("0");
+        args.push_back("low_first");
+        args.push_back("swap_bytes");
+        conv->setArgs(args);
+        MqttValue output = conv->toMqtt(input);
+
+        REQUIRE(output.getInt() == 0x02040081);
+    }
+
 
 
     SECTION("when single modbus register is converted into a string") {
